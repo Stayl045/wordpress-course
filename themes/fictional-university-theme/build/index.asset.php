89835be4b713d2ffd1c6b28b02c03778a8f6906d
Stephan Taylor <stephan.taylor@f8th.ai>
2024-10-01 11:03:46 -0600

    First commit

---
+++ themes/fictional-university-theme/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('jquery'), 'version' => 'f315084166d02c6725d0');
---
+++ themes/fictional-university-theme/src/index.js
@@ -0,0 +1,14 @@
+import "../css/style.scss"
+
+// Our modules / classes
+import Search from "./modules/Search"
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+
+
+// Instantiate a new object using our modules/classes
+const search = new Search()
+const mobileMenu = new MobileMenu()
+const heroSlider = new HeroSlider()
+
+
